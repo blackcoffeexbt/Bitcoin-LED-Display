aa5762cae6db85915f2f01fb4426d422d335871f
blackcoffeexbt <87530449+blackcoffeexbt@users.noreply.github.com>
2023-12-14 10:32:47 +0000

    Added pio build command to deploy.sh

--- ota-files/deploy.sh
+++ ota-files/deploy.sh
@@ -1,3 +1,7 @@
-cp ~/projects/lnbits/Bitcoin-LED-Display/code/.pio/build/esp32-c3-devkitm-1/firmware.bin .
+alias pio="~/.platformio/penv/bin/platformio"
+cd ../code
+pio run
+cd ../ota-files
+cp ../code/.pio/build/esp32-c3-devkitm-1/firmware.bin .
 rsync -avz . sx6-store@93.114.184.165:public/bitkoclock
 ssh sx6-store@93.114.184.165 chmod 0777 public/bitkoclock/firmware.bin
